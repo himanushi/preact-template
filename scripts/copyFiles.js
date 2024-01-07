e0188a5f4844042b4e7615e47e7d88358f328e86
himanushi <himanushi@users.noreply.github.com>
2024-01-07 20:01:41 +0900

    fix

--- scripts/copyFiles.js
+++ scripts/copyFiles.js
@@ -23,6 +23,7 @@ const excludeFiles = [
   "vite.config.ts",
   "vite-env.d.ts",
   "tsconfig.node.json",
+  "open-props.min.css",
 ];
 
 function directoryContents(dir, depth = 0) {
@@ -79,4 +80,4 @@ const contents = directoryContents(process.cwd());
 const combinedOutput = `${description}\n\n${treeOutput}\n\n${contents}`;
 
 clipboardy.writeSync(combinedOutput);
-console.log("コピーしました。");
+console.log("コピーしました");
